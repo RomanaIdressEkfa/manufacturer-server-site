9d1617fb82dcea0b4f49056324360678060e46bd
RomanaIdressEkfa <ekfaeshita@gmail.com>
2022-05-27 04:08:32 +0600

    post product completed

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ async function run() {
             const result = await productCollection.insertOne(product);
             res.send(result);
         });
+
         app.post('/order', async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
